530a6837408be13ad6f3ba0bea1f0d71b2717285
FabiolaTP <tapiafaby3@gmail.com>
2019-11-22 13:21:56 -0600

    Cambios en Grass

--- src/data.js
+++ src/data.js
@@ -3,7 +3,8 @@
 /* Manejo de data */
 
 // esta es una función de ejemplo
-import { dataPokemon } from './main.js';
+//import { dataPokemon } from './main.js';
+//import pokemon from './data/pokemon/pokemon.js';
 export const name = () => {
 //  for(let i=0; i < dataPokemon.name.length; i++){
   /*let getNames = dataPokemon.map(function(x){
@@ -16,15 +17,35 @@ export const height = () => {
   let heights = dataPokemon.filter(dataPokemon => dataPokemon.height = dataPokemon.height);
   console.log(heights);
 };
-export const grass = () => {
-  let grasses = dataPokemon.filter(dataPokemonpokemon => { dataPokemon.type.map(function(elements){
+
+export const grass = (data) => {
+  return data.filter((pokemon) => {
+    return pokemon.type[0] === 'Grass' || pokemon.type[1] === 'Grass'
+  })
+}
+  //console.log(grass(), 'grass')
+  /*grass.map((pokemon)=> {
+    let target = Document.createElement("span");
+    const template = `
+    <div class = "name"> ${pokemon.name}</div>
+    <img class = "img" src = "${pokemon.image}"/>`
+    target.innerHTML=template;
+    document.getElementById("y").appendChild(target); 
+
+  })
+console.log(grass);*/
+
+
+/*export const grass = () => {
+  let grasses = dataPokemon.filter(dataPokemon => dataPokemon.type.map(function(element){
   if(element === "Grass"){
   return dataPokemon;}
 })
-} === "Grass");
+  === "Grass"); 
 
-  console.log(grasses);
-};
+console.log(grass);
+};*/
+
 function createPokemon(){
 
 
