ec55cbbcb846c1443b1e0043383448f14dd79fec
azul-de-prusia <tegthyn_600@hotmail.com>
2019-11-26 09:43:25 -0600

    formulas filtrado

--- src/data.js
+++ src/data.js
@@ -1,52 +1,34 @@
-
-
-/* Manejo de data */
-
-// esta es una función de ejemplo
-//import { dataPokemon } from './main.js';
-//import pokemon from './data/pokemon/pokemon.js';
-export const name = () => {
-//  for(let i=0; i < dataPokemon.name.length; i++){
-  /*let getNames = dataPokemon.map(function(x){
-    return x.name*/
-  let names = dataPokemon.filter(dataPokemon => dataPokemon.name = dataPokemon.name);
-    console.log(names);
+//import { data } from './main.js';
+export const name = (data) => {
+  let names = data.filter(pokemon => data.name = data.name);
+  return names
     createPokemon()
 };
-export const height = () => {
-  let heights = dataPokemon.filter(dataPokemon => dataPokemon.height = dataPokemon.height);
-  console.log(heights);
+export const height = (data) => {
+  let heights = data.filter(pokemon => data.height = data.height);
+  return heights
+  createPokemon()
 };
 
-export const grass = (data) => {
+//let value= JSON.parse(localStorage.getItem("typesValue"));
+
+export const water = (data) => {
   return data.filter((pokemon) => {
-    return pokemon.type[0] === 'Grass' || pokemon.type[1] === 'Grass'
+    return pokemon.type[0] === 'Water' || pokemon.type[1] === "Water";
   })
 }
-  //console.log(grass(), 'grass')
-  /*grass.map((pokemon)=> {
-    let target = Document.createElement("span");
-    const template = `
-    <div class = "name"> ${pokemon.name}</div>
-    <img class = "img" src = "${pokemon.image}"/>`
-    target.innerHTML=template;
-    document.getElementById("y").appendChild(target); 
-
+export const fire = (data) => {
+  return data.filter((pokemon) => {
+    return pokemon.type[0] === 'Fire' || pokemon.type[1] === "Fire";
   })
-console.log(grass);*/
-
-
-/*export const grass = () => {
-  let grasses = dataPokemon.filter(dataPokemon => dataPokemon.type.map(function(element){
-  if(element === "Grass"){
-  return dataPokemon;}
-})
-  === "Grass"); 
-
-console.log(grass);
-};*/
-
-function createPokemon(){
-
-let name= "hola";
-};
+}
+export const flying = (data) => {
+  return data.filter((pokemon) => {
+    return pokemon.type[0] === 'Flying' || pokemon.type[1] === "Flying";
+  })
+}
+export const grass = (data) => {
+  return data.filter((pokemon) => {
+    return pokemon.type[0] === 'Grass' || pokemon.type[1] === "Grass";
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,28 +1,104 @@
 import POKEMON from './data/pokemon/pokemon.js'
-console.log(POKEMON)
  /* import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
 //import { dataPokemon } from './main.js';
 
-import { name } from './data.js';
-import {height} from './data.js';
-import { grass } from './data.js';
-//export const dataPokemon = POKEMON;
+import { name, height, water, fire, flying, grass } from './data.js';
+
+//export const data = POKEMON;
 //console.log(dataPokemon)
  /* console.log(LoL);
  * console.log(POTTER)
 */
 document.getElementById("names").addEventListener('click', name);
-document.getElementById('acces').addEventListener('click', move);
+/*document.getElementById('acces').addEventListener('click', move);*/
 document.getElementById('instruction').addEventListener("click", seeInstruction)
 function seeInstruction()
 {document.getElementById("textInstruction").innerHTML = "Hello World!";};
-function move(){
+/*function move(){
 
-}
-document.getElementById("heights").addEventListener('click', height);
-document.getElementById("grass").addEventListener('click', () => {
-    console.log(grass(POKEMON))
-    
-});
+}*/
+document.getElementById('heights').addEventListener('click', height);
+//.addEventListener('click', water);
+
+let typesValue = document.getElementById('types').value;
+console.log(typesValue)
+localStorage.setItem("typesValue", JSON.stringify(typesValue));
+
+document.getElementById('Water').addEventListener('click', () => {
+  seePokemonWater();
+})
+document.getElementById('Fire').addEventListener('click', () => {
+  seePokemonFire();
+})
+document.getElementById('Flying').addEventListener('click', () => {
+  seePokemonFlying();
+})
+document.getElementById('Grass').addEventListener('click', () => {
+  seePokemonGrass();
+})
+
+function seePokemonWater(){
+  let waterArray = water(POKEMON);
+    waterArray.map((pokemon)=> {
+
+      let target = document.createElement("div");
+      const template = `
+      <span class= "name"> ${pokemon.name}</span>
+      <input type=image class="img" src= "${pokemon.img}";/>
+      `
+      target.innerHTML = template;
+      document.getElementById("conteiner").appendChild(target);
+    }
+  )}
+  function seePokemonFire(){
+    let fireArray = fire(POKEMON);
+    console.log(fireArray);
+      fireArray.map((pokemon)=> {
+
+        let target = document.createElement("div");
+        const template = `
+        <span class= "name"> ${pokemon.name}</span>
+        <input type=image class="img" src= "${pokemon.img}";/>
+        `
+        target.innerHTML = template;
+        document.getElementById("conteiner").appendChild(target);
+      }
+    )}
+    function seePokemonFlying(){
+      let flyingArray = flying(POKEMON);
+        flyingArray.map((pokemon)=> {
+
+          let target = document.createElement("div");
+          const template = `
+          <span class= "name"> ${pokemon.name}</span>
+          <input type=image class="img" src= "${pokemon.img}";/>
+          `
+          target.innerHTML = template;
+          document.getElementById("conteiner").appendChild(target);
+        }
+      )}
+      function seePokemonGrass(){
+        let grassrArray = grass(POKEMON);
+          grassrArray.map((pokemon)=> {
+
+            let target = document.createElement("div");
+            const template = `
+            <span class= "name"> ${pokemon.name}</span>
+            <input type=image class="img" src= "${pokemon.img}";/>
+            `
+            target.innerHTML = template;
+            document.getElementById('conteiner').appendChild(target);
+          }
+        )}
+  function createPokemon(){
+  //  document.getElementById('conteiner') = '';
+    let target = document.createElement("div");
+    const template = `
+    <span class= "name"> ${pokemon.name}</span>
+    <input type=image class="img" src= "${pokemon.img}";/>
+    `
+    target.innerHTML = template;
+    document.getElementById('conteiner').appendChild(target);
+  }
